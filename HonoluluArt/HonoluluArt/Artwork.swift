1a85bdf5682e7622c62098ca6afa8642a78a101b
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-06-01 15:26:27 +0300

    add class function func fromJSON() to append data into Artwork object

--- HonoluluArt/HonoluluArt/Artwork.swift
+++ HonoluluArt/HonoluluArt/Artwork.swift
@@ -17,6 +17,7 @@ class Artwork: NSObject, MKAnnotation {
     let discipline: String
     let coordinate: CLLocationCoordinate2D
     
+    // initialier
     init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.locationName = locationName
@@ -25,6 +26,37 @@ class Artwork: NSObject, MKAnnotation {
         
         super.init()
     }
+    
+    
+    class func fromJSON(json: [JSONValue]) -> Artwork {
+        // 1 
+        var title: String
+        if let titleOrNil = json[16].string {
+            title = titleOrNil
+        } else {
+            title = ""
+        }
+        let locationName = json[12].string
+        let discipline = json[15].string
+        
+        // 2 
+        let latitude = (json[18].string! as NSString).doubleValue
+        let longitude = ( json[19].string! as NSString).doubleValue
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        // 3 
+        return Artwork(title: title, locationName: locationName!, discipline: discipline!, coordinate: coordinate)
+        
+        /* NOTES
+        1 fromJSON‘s json argument will be one of the arrays that represent an artwork – an array of JSONValue objects. If we count through an array’s elements, we'll see that the title, locationName etc. are at the indexes specified in this method. The title for some of the artworks is null so we test for this when setting the title value.
+         
+        2 This converts the string latitude and longitudes to NSString objects so we can then use the handy doubleValue to convert them to doubles.
+         
+        3 The computed string property from JSON.swift returns an optional string for locationName and discipline, which must be implicitly unwrapped when passing them to the Artwork initializer
+        */
+        
+    }
+    
     
     var subtitle: String? {
         return locationName
