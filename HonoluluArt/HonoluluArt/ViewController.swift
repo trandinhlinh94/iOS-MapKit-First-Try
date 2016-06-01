9e3743b084a828f746eae79fe6570ce8016c46b5
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-06-01 15:29:46 +0300

    add method loadInitialData() to parse JSON file and create instance object of Artwork. Later in viewDidLoad(), populate data by involking the loadInitialData(), then show the pin with mapView.addAnnotations(artworks)

--- HonoluluArt/HonoluluArt/ViewController.swift
+++ HonoluluArt/HonoluluArt/ViewController.swift
@@ -28,18 +28,74 @@ class ViewController: UIViewController {
     }
     
     
+    // PARSING JSON FILE
+    // create an array to hold Artwork objects extracting from JSON file
+    var artworks = [Artwork]()
+    
+    func loadInitialData() {
+        // 1 
+        let fileName = NSBundle.mainBundle().pathForResource("PublicArt", ofType: "json")
+        var data: NSData?
+        do {
+            data = try NSData(contentsOfFile: fileName!, options: NSDataReadingOptions(rawValue: 0))
+        } catch _ {
+            data = nil
+        }
+        
+        // 2
+        var jsonObject: AnyObject? = nil
+        if let data = data {
+            do {
+                jsonObject = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))
+            } catch _ {
+                jsonObject = nil
+            }
+        }
+        
+        // 3 
+        if let jsonObject = jsonObject as? [String: AnyObject] {
+        
+        // 4 
+            if let jsonData = JSONValue.fromObject(jsonObject)?["data"]?.array {
+                for artworkJSON in jsonData {
+                    if let artworkJSON = artworkJSON.array {
+        // 5
+                        let artwork  = Artwork.fromJSON(artworkJSON)
+                        artworks.append(artwork)
+                    }
+                }
+            }
+        }
+        
+        /*
+         
+        1 Read the PublicArt.json file into an NSData object
+         
+        2 Use NSJSONSerialization to obtain a JSON object
+         
+        3 Check that the JSON object is a dictionary where the keys are Strings and the values can be AnyObject
+         
+        4 We’re only interested in the JSON object whose key is "data" and we loop through that array of arrays, checking that each element is an array
+         
+        5 Pass each artwork’s array to the fromJSON method that you just added to the Artwork class. If it returns a valid Artwork object, we append it to the artworks array.
+ 
+        */
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         centerMapOnLocation(initialLocation)
         
-        //show a artwork on map 
-        let artwork = Artwork(title: "King David Kalakua", locationName: "Kaikiki Gateway Park", discipline: "Scrupture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
-        mapView.addAnnotation(artwork)
+        //show artworks on map
+        loadInitialData()
+        mapView.addAnnotations(artworks)
         
         mapView.delegate = self
         
     }
+    
     /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
